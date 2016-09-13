4b0c240e60a4c48634ddcf0e471cd9053dd5fe06
jmcimula <jmkacim@gmail.com>
2016-09-13 09:19:18 +0200

    Raw data *.csv with R

--- data-raw/cmsales.R
+++ data-raw/cmsales.R
@@ -2,3 +2,6 @@ library(readr)
 
 cmsales <- read_csv("data-raw/cmsales.csv")
 devtools::use_data(cmsales, overwrite = TRUE)
+
+cmstelecom <- read_csv("data-raw/cmstelecom.csv")
+devtools::use_data(cmstelecom, overwrite = TRUE)
